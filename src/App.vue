4f8a98aacdd7783ccc2ea74aa1b984118b1eac1e
victor-villafane <victor.villafane@davinci.edu.ar>
2025-11-03 21:56:56 -0300

    View - Add - agregue una nueva vista

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <ul>
+    <li>
+      <RouterLink to="/">Home</RouterLink>
+    </li>
+  </ul>
+
+  <RouterView />
 </template>
 
 <style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,1 @@
+<template>Hola!</template>
